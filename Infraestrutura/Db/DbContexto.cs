8bcc89c47e4cccbabd24d95a1198c6974818d4f0
Carlos Amaral <chayimamaral@gmail.com>
2025-09-21 19:24:42 -0300

    Login desenvolvido

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -9,11 +9,43 @@ namespace minimal_api.Infraestrutura.Db
 {
     public class DbContexto : DbContext
     {
+
+        private readonly IConfiguration _configurationAppSettings;
+        // public DbContexto(IConfiguration configurationAppSettings)
+        // {
+        //     _configurationAppSettings = configurationAppSettings;
+
+        // }
+        public DbContexto(DbContextOptions<DbContexto> options) : base(options) { }
+
         public DbSet<Administrador> Administradores { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Administrador>().HasData(
+                new Administrador
+                {
+                    Id = 1,
+                    Email = "adm@teste.com",
+                    Senha = "123456",
+                    Perfil = "Adm"
+                });
+
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=minimal_api;Username=postgres;Password=123456");
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                var stringConexao = _configurationAppSettings.GetConnectionString("PostgreSqlConnection")?.ToString();
+                if (!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseNpgsql(stringConexao);
+                }
+                throw new Exception("String de conexão nula");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using minimal_api.Infraestrutura.Db;
+using minimal_api.Dominio.DTOs;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.Interfaces;
+using minimal_api.Dominio.Servicos; // Add this line if AdministradorServico is here
+using Microsoft.AspNetCore.Mvc;
 
+var builder = WebApplication.CreateBuilder(args);
+
+IServiceCollection serviceCollection = builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+  options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection"),
+  npgsqlOptions =>
+  {
+    npgsqlOptions.EnableRetryOnFailure(
+          maxRetryCount: 5,
+          maxRetryDelay: TimeSpan.FromSeconds(10),
+          errorCodesToAdd: null);
+  });
+});
+
+var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (minimal_api.Dominio.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-  if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+  if (administradorServico.Login(loginDTO) != null)
 
     return Results.Ok("Login com sucesso");
   else
